efcf84d7718a661e6857bbab8e2b188f609cda8a
Bruno Melo <utibmelo@gmail.com>
2020-11-05 12:39:09 -0300

    chore: create server

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import cors from 'cors'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import schema from './modules/buildTypeDefs'
+import resolvers from './modules/buildResolver'
+import connectDatabase from './config/database'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: makeExecutableSchema({
+      typeDefs: schema,
+      resolvers
+    }),
+    graphiql: true
+  })
+)
+
+connectDatabase()
+  .then(() => 
+    app.listen(3333)
+  ).then(() => 
+    console.log(`server listening`)
+  )
