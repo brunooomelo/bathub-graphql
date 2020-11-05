bf3611f012cf2993fba00bb83d91405b2c2f2b02
Bruno Melo <utibmelo@gmail.com>
2020-11-05 12:38:47 -0300

    chore: add user model

---
+++ src/models/users.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false
+    },
+    passwordResetToken: {
+      type: String,
+      select: false
+    },
+    passwordResetExpires: {
+      type: Date,
+      select: false
+    },
+    isActive: {
+      type: Boolean,
+      default: true
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+    autoIndex: true
+  }
+)
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 9);
+})
+
+module.exports = mongoose.model('users', userSchema)
