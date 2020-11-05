55225a77bccff51c7dbe06c5a581dfe5db371d90
Bruno Melo <utibmelo@gmail.com>
2020-11-05 14:44:30 -0300

    feat: add queries and mutation for user

--- src/modules/users/resolvers.js
+++ src/modules/users/resolvers.js
@@ -1,12 +1,11 @@
-import User from '../../database/models/users'
+import User from '../../models/users'
+import mail from '../../services/mail'
+import crypto from 'crypto'
 
 export default {
   Query :{
-    getUser: () => ({
-      username: 'String',
-      email: 'String',
-      password: 'String'
-    })
+    getUsers: async () => User.find().lean(),
+    getUser: async (_, { id }) => User.findOne({ _id: id }).lean()
   },
   Mutation: {
     createUser: async (_, { data }) => {
@@ -18,14 +17,66 @@ export default {
         }]
       }).lean()
 
-
       // review default error
       if (duplicate) {
         return null
       }
-      
+
       const response = await User.create(data)
       return response
+    },
+    activeUser: async (_, { id }) => {
+      const response = await User.updateOne({ _id: id }, { isActive: true })
+      return response
+    },
+    disableUser: async (_, { id }) => {
+      const response = await User.updateOne({ _id: id }, { isActive: false })
+      return response
+    },
+    forgotPassword: async (_, { credentials }) => {
+      const user = await User.findOne({
+        $or: [{
+          email: credentials
+        }, {
+          username: credentials
+        }]
+      }).select('+passwordResetToken +passwordResetExpires')
+
+      if (!user) {
+        return false
+      }
+
+      const token = crypto.randomBytes(20).toString('hex')
+      const now = new Date()
+      now.setHours(now.getHours() + 1)
+
+      user.passwordResetToken = token
+      user.passwordResetExpires = now
+
+      await user.save()
+
+      await mail.send()
+
+      return true
+    },
+    resetPassword: async (_, { data }) => {
+      const user = await User.findOne({
+        passwordResetToken: data.token
+      }).select('+passwordResetToken +passwordResetExpires')
+
+      const now = new Date()
+
+      if (now > user.passwordResetExpires) {
+        return false
+      }
+
+      user.password = data.password
+      user.passwordResetExpires = undefined
+      user.passwordResetToken = undefined
+
+      await user.save()
+
+      return true
     }
   }
 }
